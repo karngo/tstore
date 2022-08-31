d811556cff0f35ac64108b565886bf252ff610d4
karna <karnagogoi7@gmail.com>
2022-08-31 16:39:28 +0530

    Add filter component with checkbox

---
+++ src/components/CheckFilter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-subheader v-if="title"> {{ title }} </v-subheader>
+    <v-list-item-group color="primary" v-model="selectedOptions" multiple>
+      <v-list-item
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option"
+      >
+        <v-checkbox
+          dense
+          class="text-caption"
+          :value="selectedOptions.includes(option)"
+        >
+          <template v-slot:label>
+            <v-list-item-title>
+              {{ option.toUpperCase() }}
+            </v-list-item-title>
+          </template>
+        </v-checkbox>
+      </v-list-item>
+    </v-list-item-group>
+  </div>
+</template>
+<script>
+export default {
+  name: "CheckFilter",
+  props: {
+    title: String,
+    options: Array,
+  },
+  data() {
+    return {
+      selectedOptions: [],
+    };
+  },
+  watch: {
+    selectedOptions() {
+      this.$emit("change", this.selectedOptions);
+    },
+  },
+};
+</script>
