4ca0693f0ef4eb8d6a1c2beb11ab3673e829a8da
karna <karnagogoi7@gmail.com>
2022-08-31 11:56:39 +0530

    Filter products list on search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-text-field placeholder="Search for Products...">
+        <v-text-field
+          placeholder="Search for Products..."
+          v-model="searchTerm"
+          v-on:keydown.enter.stop="searchProducts"
+        >
           <template v-slot:append-outer>
-            <v-btn tile elevation="0">
+            <v-btn tile elevation="0" @click="searchProducts">
               <v-icon>mdi-magnify</v-icon>
             </v-btn>
           </template>
@@ -13,7 +17,7 @@
     </v-row>
     <v-row>
       <v-col class="pa-0">
-        <Catalogue :items="products" />
+        <Catalogue :items="displayedProducts" />
       </v-col>
     </v-row>
   </v-container>
@@ -27,14 +31,38 @@ export default {
   components: {
     Catalogue,
   },
+  data() {
+    return {
+      searchTerm: "",
+      displayedProducts: [],
+    };
+  },
   computed: {
     ...mapState(["products"]),
   },
-  mounted() {
-    this.fetchProducts();
+  async mounted() {
+    await this.fetchProducts();
+    this.displayedProducts = this.products;
   },
   methods: {
     ...mapActions(["fetchProducts"]),
+    searchProducts() {
+      const searchKeys = this.searchTerm.trim().split(" ");
+
+      if (!searchKeys.length) {
+        this.displayedProducts = this.products;
+      }
+
+      const searchedProducts = this.products.filter(({ name, type, color }) => {
+        return searchKeys.every((searchKey) => {
+          return [name, type, color].some((productKey) =>
+            productKey.toLowerCase().includes(searchKey.toLowerCase())
+          );
+        });
+      });
+
+      this.displayedProducts = searchedProducts;
+    },
   },
 };
 </script>
