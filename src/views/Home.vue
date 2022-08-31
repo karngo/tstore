f52cfd71071f0c10fd42df919c852efbf5e22ee8
karna <karnagogoi7@gmail.com>
2022-08-31 11:17:02 +0530

    Update home page to show products in catalogue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,29 @@
 <template>
-  <h2>Tstore App</h2>
+  <v-container>
+    <v-row>
+      <v-col class="pa-0">
+        <Catalogue :items="products" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
+import Catalogue from "../components/Catalogue.vue";
+import { mapActions, mapState } from "vuex";
+
 export default {
   name: "Home",
+  components: {
+    Catalogue,
+  },
+  computed: {
+    ...mapState(["products"]),
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    ...mapActions(["fetchProducts"]),
+  },
 };
 </script>
