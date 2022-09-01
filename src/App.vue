be6bc2e3b0c2d40f7f5711f1f52914673b15c112
karna <karnagogoi7@gmail.com>
2022-09-01 14:21:42 +0530

    Fix app bar on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <v-app>
+    <v-app-bar app color="deep-purple accent-4" class="px-6">
+      <v-toolbar-title>TeeRex Store</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text to="/"> Products </v-btn>
+      <v-btn icon to="/cart">
+        <v-badge
+          color="green"
+          :value="cartItems.length"
+          :content="cartItems.length"
+          overlap
+        >
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
     <v-main>
-      <v-app-bar color="deep-purple accent-4" class="px-6">
-        <v-toolbar-title>TeeRex Store</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn text to="/"> Products </v-btn>
-        <v-btn icon to="/cart">
-          <v-badge
-            color="green"
-            :value="cartItems.length"
-            :content="cartItems.length"
-            overlap
-          >
-            <v-icon>mdi-cart</v-icon>
-          </v-badge>
-        </v-btn>
-      </v-app-bar>
-
       <router-view />
     </v-main>
   </v-app>
